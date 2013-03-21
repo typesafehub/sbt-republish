e2c4645ece304146c89ccf2fa5d313237595cba7
Peter Vlugter <pvlugter@gmail.com>
2013-03-21 19:18:22 +1300

    Republish sbt 0.13.0-M1

--- project/SbtRepublish.scala
+++ project/SbtRepublish.scala
@@ -15,7 +15,7 @@ object SbtRepublish extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.typesafe.sbt",
-    version := "0.13.0-SNAPSHOT",
+    version := "0.13.0-M1",
     scalaVersion := "2.10.1",
     originalSbtVersion <<= version { v => if (v.endsWith("SNAPSHOT")) "latest.integration" else v },
     resolvers <++= version { v => if (v.endsWith("SNAPSHOT")) Seq(Classpaths.typesafeSnapshots) else Seq.empty },
