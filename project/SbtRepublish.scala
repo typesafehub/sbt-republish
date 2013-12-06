1aff0cf869825e087dc8299c651dee3b4653d756
Peter Vlugter <pvlugter@gmail.com>
2013-12-06 16:16:11 +1300

    Update to scala 2.10.3

--- project/SbtRepublish.scala
+++ project/SbtRepublish.scala
@@ -18,7 +18,7 @@ object SbtRepublish extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.typesafe.sbt",
     version := "0.13.1-SNAPSHOT",
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
     originalSbtVersion <<= version { v => if (v.endsWith("SNAPSHOT")) "latest.integration" else v },
     resolvers <++= version { v => if (v.endsWith("SNAPSHOT")) Seq(Classpaths.typesafeSnapshots) else Seq.empty },
     resolvers += Classpaths.typesafeReleases,
